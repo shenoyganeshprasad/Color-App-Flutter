07f184891f24a32ef56087043f81c45c782b8db7
Ganeshprasad Shenoy <shenoyganeshprasad@gmail.com>
2020-03-20 12:24:00 +0530

    Add snackbar

--- lib/widgets/colorcontainer.dart
+++ lib/widgets/colorcontainer.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class ColorContainer extends StatelessWidget {
   final Color bgColor;
   final String hexValue;
+  final snackBar = SnackBar(
+    behavior: SnackBarBehavior.floating,
+    content: Text('Copied to Clipboard'),
+  );
   ColorContainer({@required this.bgColor, @required this.hexValue});
   @override
   Widget build(BuildContext context) {
@@ -14,14 +19,19 @@ class ColorContainer extends StatelessWidget {
         child: Center(
           child: Opacity(
             opacity: 0.8,
-            child: Text(
-              "#" + hexValue,
-              style: GoogleFonts.lato(
-                  textStyle: TextStyle(
-                fontSize: 32.0,
-                color: Colors.white,
-                fontWeight: FontWeight.bold
-              )),
+            child: GestureDetector(
+              onLongPress: () {
+                Clipboard.setData(new ClipboardData(text: hexValue));
+                Scaffold.of(context).showSnackBar(snackBar);
+              },
+              child: Text(
+                "#" + hexValue,
+                style: GoogleFonts.lato(
+                    textStyle: TextStyle(
+                        fontSize: 32.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold)),
+              ),
             ),
           ),
         ),
